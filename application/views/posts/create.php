126d2649e01a87713ae4f4394f940c995b3b2244
Nhut Le <leminhnhut08t1@gmail.com>
2019-08-05 15:43:02 +0700

    part 3

--- application/views/posts/create.php
+++ application/views/posts/create.php
@@ -2,7 +2,7 @@
 
 <?php echo validation_errors(); ?>
 
-<?php echo form_open('posts/create'); ?>
+<?php echo form_open_multipart('posts/create'); ?>
     <div class="form-group">
         <label>Title</label>
         <input type="text" class="form-control" name="title" placeholder="Add Title">
@@ -10,6 +10,18 @@
     <div class="form-group">
         <label>Body</label>
         <textarea name="body" class="form-control" placeholder="Add Body" id="editor1"></textarea>
+    </div>
+    <div class="form-group">
+        <label>Category</label>
+        <select name="category_id" class="form-control">
+            <?php foreach ($categories as $category) { ?>
+                <option value="<?php echo $category['id']; ?>"><?php echo $category['name']; ?></option>
+            <?php } ?>
+        </select>
+    </div>
+    <div class="form-group">
+        <label>Upload Image</label>
+        <input type="file" name="userfile" size="20">
     </div>
     <button type="submit" class="btn btn-default">Submit</button>
 <?php echo form_close(); ?>
--- application/views/posts/edit.php
+++ application/views/posts/edit.php
@@ -3,14 +3,22 @@
 <?php echo validation_errors(); ?>
 
 <?php echo form_open('posts/update'); ?>
-<input type="hidden" name="id" value="<?php echo $post['id']; ?>">
-<div class="form-group">
-    <label>Title</label>
-    <input type="text" class="form-control" name="title" placeholder="Add Title" value="<?php echo $post['title']; ?>">
-</div>
-<div class="form-group">
-    <label>Body</label>
-    <textarea name="body" class="form-control" placeholder="Add Body" id="editor1"><?php echo $post['body']; ?></textarea>
-</div>
-<button type="submit" class="btn btn-default">Submit</button>
+    <input type="hidden" name="id" value="<?php echo $post['id']; ?>">
+    <div class="form-group">
+        <label>Title</label>
+        <input type="text" class="form-control" name="title" placeholder="Add Title" value="<?php echo $post['title']; ?>">
+    </div>
+    <div class="form-group">
+        <label>Body</label>
+        <textarea name="body" class="form-control" placeholder="Add Body" id="editor1"><?php echo $post['body']; ?></textarea>
+    </div>
+    <div class="form-group">
+        <label>Category</label>
+        <select name="category_id" class="form-control">
+            <?php foreach ($categories as $category) { ?>
+                <option <?php if ($post['category_id'] == $category['id']) echo "selected"; ?> value="<?php echo $category['id']; ?>"><?php echo $category['name']; ?></option>
+            <?php } ?>
+        </select>
+    </div>
+    <button type="submit" class="btn btn-default">Submit</button>
 <?php echo form_close(); ?>
