2b607028e4945e98b31a71506bae848862b3c316
Nhut Le <leminhnhut08t1@gmail.com>
2019-08-05 17:29:28 +0700

    part 5

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -8,5 +8,37 @@
 <hr>
 <a class="btn btn-default pull-left" href="<?php echo base_url();?>posts/edit/<?php echo $post['slug']; ?>">Edit</a>
 <?php echo form_open('/posts/delete/' . $post['id']); ?>
-<input type="submit" value="Delete" class="btn btn-danger">
+    <input type="submit" value="Delete" class="btn btn-danger">
+<?php echo form_close(); ?>
+
+<hr>
+<h3>Comments</h3>
+<?php if ($comments) { ?>
+    <?php foreach ($comments as $comment) { ?>
+        <div class="well">
+            <h5><?php echo $comment['body']; ?> [by <strong><?php echo $comment['name']; ?></strong>]</h5>
+        </div>
+    <?php } ?>
+<?php } else { ?>
+    <p>No Comments To Display</p>
+<?php } ?>
+
+<hr>
+<h3>Add Comment</h3>
+<?php echo validation_errors(); ?>
+<?php echo form_open('/comments/create/'.$post['id']); ?>
+    <input type="hidden" name="slug" value="<?php echo $post['slug']; ?>">
+    <div class="form-group">
+        <label>Name</label>
+        <input type="text" name="name" class="form-control">
+    </div>
+    <div class="form-group">
+        <label>Email</label>
+        <input type="text" name="email" class="form-control">
+    </div>
+    <div class="form-group">
+        <label>Body</label>
+        <textarea name="body" class="form-control"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
 <?php echo form_close(); ?>
